37a1f9d4bcf5765dbedd94a0c598201971539a72
simonb2527 <global>
2022-09-22 10:30:32 -0400

    web changes

--- src/main.php
+++ src/main.php
@@ -1,45 +1,25 @@
-<?php
 
-session_start();
-$status = $_SESSION["loggedIn"];
-
-?>
 <html>
 
-    <head>
-    <style>
-body {background-color: powderblue;}
-h1   {color: blue;}
-p    {color: red;}
+<head>
+<style>
+body {background-color: white;}
+h1   {color: white; margin: 20px; 
+background-color: blue;}
+
+
+
 </style>
+<h1> Online ordering </h1>
+<img src= "images/delivery bird.webp" >
+
 </head>
+
+
 <body>
 
 
 
-
-
-    <?php
-    if($loggedIn != "YES"){
-
-    ?>
-    <img src= "images/delivery bird.webp" >
-
-    <form method = "post" action = "login.php" >
-    Username: <input type= "text" name="username"> <br>
-</br>
-    Password: <input type= "password" name= "password">
-    <br>
-</br>
-    <input type= "submit" value= "Login" >
-<?php
-    }else { echo"Welcome! Click here <a href = 'secret.php'> </a>for intfo on company picnic"}
-?>
-
-</form>
-
-    
-    
 
 </body>
 </html>
