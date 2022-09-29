1cad402464d00eddf0ea39783f3d66b28077a220
August Bednar <bednara@etown.edu>
2022-09-29 08:39:29 -0400

    added header and footer to homepage

--- src/home.php
+++ src/home.php
@@ -1,3 +1,8 @@
+<?PHP
+$path = '';
+require("includes/header.php");
+
+?>
  <!-- Header -->
  <div class="w3-container" style="margin-top:80px" id="showcase">
     <h1 class="w3-jumbo"><b>Jays Nest Rovotic Delivery</b></h1>
@@ -8,3 +13,6 @@
   <?php
   echo "Welcome to the main page of the Jays Nest Delivery Website.";
   ?>
+
+<?PHP
+require("includes/footer.php");
