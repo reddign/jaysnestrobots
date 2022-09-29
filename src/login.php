2b4bdb3eeca53939b08b7422496b53d64bd697e9
simonb2527 <global>
2022-09-29 09:58:25 -0400

    login test p2

--- src/login.php
+++ src/login.php
@@ -4,9 +4,16 @@ session_start();
 $status = $_SESSION["loggedIn"];
 
 ?>
+<?PHP
+$path = '';
+require("includes/header.php");
+?>
 
-<div class="w3-container" style="margin-top:80px" id="showcase">
-    <h1 class="w3-jumbo"><b>Jays Nest Robotic Delivery</b></h1>
-    <h1 class="w3-xxxlarge w3-text-red"><b>Database.</b></h1>
+ <!-- Header -->
+ <div class="w3-container" style="margin-top:80px" id="showcase">
+    <h1 class="w3-jumbo"><b>Menu</b></h1>
     <hr style="width:50px;border:5px solid red" class="w3-round">
  </div>
+
+ <?PHP
+require("includes/footer.php");
