f7f11858595d4a02e8e1acd227410e3255387fc3
simonb2527 <global>
2022-09-29 09:57:09 -0400

    testing login page css

---
+++ src/login.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+$status = $_SESSION["loggedIn"];
+
+?>
+
+<div class="w3-container" style="margin-top:80px" id="showcase">
+    <h1 class="w3-jumbo"><b>Jays Nest Robotic Delivery</b></h1>
+    <h1 class="w3-xxxlarge w3-text-red"><b>Database.</b></h1>
+    <hr style="width:50px;border:5px solid red" class="w3-round">
+ </div>
