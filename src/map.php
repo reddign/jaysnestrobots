a411b8ecc4534a961499b6a267d9e79b83e0691f
August Bednar <bednara@etown.edu>
2022-10-25 10:35:15 -0400

    improved map page

--- src/map.php
+++ src/map.php
@@ -9,12 +9,23 @@ require("includes/header.php");
     <hr style="width:50px;border:5px solid red" class="w3-round">
  </div>
 
+ <?php
+  echo "This is the map of current Jay's Nest delivery locations on the Elizabethtown College Campus.";
+  ?>
+
 <!-- Photo grid (modal) -->
-<div class="w3-row-padding">
+<div>
     <div class="w3-half">
-      <img src="images/map.jpg" style="width:100%" onclick="onClick(this)" alt="Delivery Locations on Campus Map">
+      <img src="images/map.jpg" style="width:175%" onclick="onClick(this)" alt="Delivery Locations on Campus Map">
     </div>
 
+    <div id="modal01" class="w3-modal w3-black" style="padding-top:0" onclick="this.style.display='none'">
+    <span class="w3-button w3-black w3-xxlarge w3-display-topright">x</span>
+    <div class="w3-modal-content w3-animate-zoom w3-center w3-transparent w3-padding-64">
+      <img id="img01" class="w3-image">
+      <p id="caption"></p>
+    </div>
+</div>
 
  <?PHP
 require("includes/footer.php");
