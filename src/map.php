b8b78b9d6038adab9ffb67061ee468212f499f6f
August Bednar <bednara@etown.edu>
2022-10-25 10:29:02 -0400

    added map to map page

--- src/map.php
+++ src/map.php
@@ -1,7 +1,6 @@
 <?PHP
 $path = '';
 require("includes/header.php");
-//Once we develpo a list of locations for delivery we can make a map and highlight these locations
 ?>
 
  <!-- Header -->
@@ -9,6 +8,13 @@ require("includes/header.php");
     <h1 class="w3-jumbo"><b>Map</b></h1>
     <hr style="width:50px;border:5px solid red" class="w3-round">
  </div>
+
+<!-- Photo grid (modal) -->
+<div class="w3-row-padding">
+    <div class="w3-half">
+      <img src="images/map.jpg" style="width:100%" onclick="onClick(this)" alt="Delivery Locations on Campus Map">
+    </div>
+
 
  <?PHP
 require("includes/footer.php");
