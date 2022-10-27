c3bc2c9699e1721868325d506bc3fb134511b690
August Bednar <bednara@etown.edu>
2022-10-27 10:17:57 -0400

    updated menu page

--- src/menu.php
+++ src/menu.php
@@ -1,6 +1,7 @@
 <?PHP
 $path = '';
 require("includes/header.php");
+require("functions/menu_functions.php");
 //This page will be worked on once we have a definite list of items to build a menu
 ?>
 
@@ -9,8 +10,22 @@ require("includes/header.php");
     <h1 class="w3-jumbo"><b>Menu</b></h1>
     <hr style="width:50px;border:5px solid red" class="w3-round">
  </div>
-
- <iframe width="1000" height="500" frameborder="0" scrolling="no" src="https://elizabethtown-my.sharepoint.com/:x:/g/personal/bednara_etown_edu/EffX_7Ze_-dGrozdANRRUoMBvyNfCKLhk3K6CDkCpMZDgg?e=kP7HCp&action=embedview&wdbipreview=true">
- 
  <?PHP
+//Sets the page value for display
+$page = isset($_GET["page"])?$_GET["page"]:"mto";
+
+
+display_student_page_navigation("Menu");
+switch($page){
+   case "mto":
+     $string = isset($_GET["mto"])?$_GET["mto"]:"";
+     display_mto_menu();
+     break;
+   case "snd":
+     display_snd_menu();
+     break;
+ }
+
+ 
+ 
 require("includes/footer.php");
