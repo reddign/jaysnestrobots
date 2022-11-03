393038ebc3d3b7048d5f41cb71419ab65d136a81
August Bednar <bednara@etown.edu>
2022-11-03 08:55:57 -0400

    rework of menu page

--- src/menu.php
+++ src/menu.php
@@ -3,16 +3,7 @@ $path = '';
 require("includes/header.php");
 require("functions/menu_functions.php");
 //This page will be worked on once we have a definite list of items to build a menu
-?>
 
- <!-- Header -->
- <div class="w3-container" style="margin-top:80px" id="showcase">
-    <h1 class="w3-jumbo"><b>Menu</b></h1>
-    <hr style="width:50px;border:5px solid red" class="w3-round">
- </div>
-
- <iframe width="1000" height="500" frameborder="0" scrolling="no" src="https://elizabethtown-my.sharepoint.com/:x:/g/personal/bednara_etown_edu/EffX_7Ze_-dGrozdANRRUoMBvyNfCKLhk3K6CDkCpMZDgg?e=kP7HCp&action=embedview&wdbipreview=true">
-<?php
  echo "Large bag of chips
  Small bag of chips
  16 oz bottle of soda
@@ -27,8 +18,8 @@ require("functions/menu_functions.php");
  Quest protein bars
  Skittles
  Snickers";
-?>
- <?PHP
+
+ 
 //Sets the page value for display
 $page = isset($_GET["page"])?$_GET["page"]:"mto";
 display_menu_page_navigation("Menu");
