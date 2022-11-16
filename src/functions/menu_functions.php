466972dc0dd72486a7dbd6afc8a412d0bae122ec
August Bednar <bednara@etown.edu>
2022-11-16 12:31:57 -0500

    added 'add to cart button' to menu page

--- src/functions/menu_functions.php
+++ src/functions/menu_functions.php
@@ -14,11 +14,39 @@ function get_food_by_name($word){
         return get_all_food_from_db();
     }
     $pdo = connect_to_db();
-    $stmt = $pdo->prepare("SELECT * FROM student WHERE firstName like :name or lastName like :name");
+    $stmt = $pdo->prepare("SELECT * FROM items WHERE description like :name");
     $stmt->execute([':name' => $word."%"]);
     $data = [];
-    while($student =  $stmt->fetch(PDO::FETCH_ASSOC)){
-        $data[] = $student;
+    while($food =  $stmt->fetch(PDO::FETCH_ASSOC)){
+        $data[] = $food;
+    } 
+    
+    return $data;
+}
+function get_mto_by_name($word){
+    if($word==""){
+        return get_all_mto_from_db();
+    }
+    $pdo = connect_to_db();
+    $stmt = $pdo->prepare("SELECT * FROM items WHERE category = 'MTO' and description like :name");
+    $stmt->execute([':name' => $word."%"]);
+    $data = [];
+    while($food =  $stmt->fetch(PDO::FETCH_ASSOC)){
+        $data[] = $food;
+    } 
+    
+    return $data;
+}
+function get_snd_by_name($word){
+    if($word==""){
+        return get_all_snd_from_db();
+    }
+    $pdo = connect_to_db();
+    $stmt = $pdo->prepare("SELECT * FROM items WHERE category = 'SND' and description like :name");
+    $stmt->execute([':name' => $word."%"]);
+    $data = [];
+    while($food =  $stmt->fetch(PDO::FETCH_ASSOC)){
+        $data[] = $food;
     } 
     
     return $data;
@@ -28,8 +56,18 @@ function get_all_food_from_db(){
     $data = $pdo->query("SELECT * FROM items order by category,ftype")->fetchAll();
     return $data;
 }
+function get_all_mto_from_db(){
+    $pdo = connect_to_db();
+    $data = $pdo->query("SELECT * FROM items WHERE category = 'MTO' order by ftype")->fetchAll();
+    return $data;
+}
+function get_all_snd_from_db(){
+    $pdo = connect_to_db();
+    $data = $pdo->query("SELECT * FROM items WHERE category = 'SND' order by ftype")->fetchAll();
+    return $data;
+}
 function display_search_form(){
-    echo '<h2>Search for a food item by Name</h2><form method=get action="menu.php">
+    echo '<h2>Search for an Item by Name</h2><form method=get action="menu.php">
         Enter Food Name:<input name="search" type="text">
         <input name="page" type="hidden" value="search">
         <input type="submit" value="Search">
@@ -42,8 +80,16 @@ function display_food_list($data=null){
         return;
     }
     foreach ($data as $row) {
-            echo "<a href='menu.php?page=menu&iid=".$row['itemID']."'>";
-            echo "$".$row['price']." ".$row['description']."<br />\n";
-            echo "</a>";
+
+        echo '<input type="submit" value="Add to Cart" name="add_to_cart">';
+        echo "\t";
+        echo "<a>";
+        echo "$".number_format((float)$row['price'], 2, '.', '');
+        echo "\t\t".$row['description']."<br />\n";
+        echo "</a>";
+        //if(isset($_POST['add_to_cart'])){
+        //    echo 'added '.$data['description'].' to cart';
+        //}        
     }
 }
+
