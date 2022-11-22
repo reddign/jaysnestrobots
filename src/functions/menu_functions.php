84c60293f7d7175ac85ac43b1196e411fca6e3b5
August Bednar <bednara@etown.edu>
2022-11-22 09:28:35 -0500

    updated sql insert from menu to cart

--- src/functions/menu_functions.php
+++ src/functions/menu_functions.php
@@ -66,14 +66,7 @@ function get_all_snd_from_db(){
     $data = $pdo->query("SELECT * FROM items WHERE category = 'SND' order by ftype")->fetchAll();
     return $data;
 }
-function display_search_form(){
-    echo '<h2>Search for an Item by Name</h2><form method=get action="menu.php">
-        Enter Food Name:<input name="search" type="text">
-        <input name="page" type="hidden" value="search">
-        <input type="submit" value="Search">
-    </form><br/><br/>';
 
-}
 function display_food_list($data=null){
     if(!is_array($data)){
         echo "";
@@ -81,15 +74,17 @@ function display_food_list($data=null){
     }
     foreach ($data as $row) {
 
-        echo '<input type="submit" value="Add to Cart" name="add_to_cart">';
+        echo '<form method="post"><input type="submit" name="$row" value="Add to Cart" ></form>';
         echo "\t";
         echo "<a>";
         echo "$".number_format((float)$row['price'], 2, '.', '');
         echo "\t\t".$row['description']."<br />\n";
         echo "</a>";
-        //if(isset($_POST['add_to_cart'])){
-        //    echo 'added '.$data['description'].' to cart';
-        //}        
+        if(isset($_POST['$row'])){
+            //echo 'added '.$row['description'].' to cart';
+            "INSERT INTO cart(price, description, weight) VALUES (:price, :description,:weight)";
+        }        
     }
 }
+
 
